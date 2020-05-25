6af4690f1b7ac404a94ffd5ee4b2ca66b31577c0
María Lourdes <37163754+narommm@users.noreply.github.com>
2020-05-25 13:11:56 -0600

    Vistas 1.2

--- app/src/main/java/com/example/pdmproyecto/operaciones_basicas.kt
+++ app/src/main/java/com/example/pdmproyecto/operaciones_basicas.kt
@@ -6,20 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-
-class Registro : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+/**
+ * A simple [Fragment] subclass.
+ */
+class operaciones_basicas : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registro, container, false)
+        return inflater.inflate(R.layout.fragment_operaciones_basicas, container, false)
     }
 
 }
