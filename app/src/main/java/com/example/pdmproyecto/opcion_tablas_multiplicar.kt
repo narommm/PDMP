13fa4ff3febcd3b099244fcb6606f2f98fbc401d
María Lourdes <37163754+narommm@users.noreply.github.com>
2020-05-27 15:11:06 -0600

    Apply Navigation

---
+++ app/src/main/java/com/example/pdmproyecto/opcion_tablas_multiplicar.kt
@@ -0,0 +1,22 @@
+package com.example.pdmproyecto
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class opcion_tablas_multiplicar : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_opcion_tablas_multiplicar, container, false)
+    }
+
+}
--- app/src/main/java/com/example/pdmproyecto/operaciones_basicas.kt
+++ app/src/main/java/com/example/pdmproyecto/operaciones_basicas.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.pdmproyecto.databinding.FragmentOpcionesInicialesBinding
+import androidx.navigation.findNavController
+import com.example.pdmproyecto.databinding.FragmentOperacionesBasicasBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -15,8 +19,20 @@ class operaciones_basicas : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_operaciones_basicas, container, false)
+        var binding = DataBindingUtil.inflate<FragmentOperacionesBasicasBinding>(inflater, R.layout.fragment_operaciones_basicas, container,false)
+        binding.opcionSuma.setOnClickListener {
+            it.findNavController().navigate(R.id.action_operaciones_basicas_to_ejemplo_suma_1)
+        }
+        binding.opcionResta.setOnClickListener {
+            it.findNavController().navigate(R.id.action_operaciones_basicas_to_resta_explicacion_uno)
+        }
+        binding.opcionMultiplicacion.setOnClickListener {
+            it.findNavController().navigate(R.id.action_opciones_iniciales_to_registro_actividades)
+        }
+        binding.opcionDivision.setOnClickListener {
+            it.findNavController().navigate(R.id.action_opciones_iniciales_to_registro_actividades)
+        }
+        return binding.root
     }
 
 }
