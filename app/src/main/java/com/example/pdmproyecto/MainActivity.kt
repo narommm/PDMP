6952ac6a06cb576580f6aceab8a5d45e2b798f28
AlxMancia <00305018@uca.edu.sv>
2020-05-27 21:11:59 -0600

    Add drawer menu base

--- app/src/main/java/com/example/pdmproyecto/MainActivity.kt
+++ app/src/main/java/com/example/pdmproyecto/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.pdmproyecto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
