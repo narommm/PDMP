c313192d0039be6938a004212fe54cf5eab66c0a
María Lourdes <37163754+narommm@users.noreply.github.com>
2020-07-02 23:31:22 -0600

    Preguntas juego

---
+++ app/src/main/java/com/example/pdmproyecto/nivel_bajo_pregunta_2.kt
@@ -0,0 +1,22 @@
+package com.example.pdmproyecto
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class nivel_bajo_pregunta_2 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_nivel_bajo_pregunta_2, container, false)
+    }
+
+}
